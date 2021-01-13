b98124438ea1a5d574a1d150dd0c4ebb3c28dc99
fOrDiseC <72049377+fOrDiseC@users.noreply.github.com>
2021-01-13 18:41:29 +0300

    Add files via upload

---
+++ adapter_pattern/second_variant/PageListPrinter.java
@@ -0,0 +1,12 @@
+package patterns.structural.adapter_pattern.second_variant;
+
+import java.util.List;
+
+/**
+ * Created on 21. November. 16.
+ *
+ * @author Evgeniy
+ */
+public interface PageListPrinter {
+    void print(List<String> list);
+}
