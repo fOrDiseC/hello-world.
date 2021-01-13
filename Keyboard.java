df4f55bcd8965332dfe8cade0dd669fa9f86f5bb
fOrDiseC <72049377+fOrDiseC@users.noreply.github.com>
2021-01-13 18:40:01 +0300

    Add files via upload

---
+++ Keyboard.java
@@ -0,0 +1,11 @@
+package patterns.creational.abstract_factory_pattern.first_variant;
+
+/**
+ * Created on 18. November. 16.
+ *
+ * @author Evgeniy
+ */
+public interface Keyboard {
+    void print();
+    void println();
+}
---
+++ TouchPad.java
@@ -0,0 +1,10 @@
+package patterns.creational.abstract_factory_pattern.first_variant;
+
+/**
+ * Created on 18. November. 16.
+ *
+ * @author Evgeniy
+ */
+public interface TouchPad {
+    void track(int deltaX, int deltaY);
+}
